97f609a0dc4b97eb635e0aa3d97ccd4c9c60bb17
jandress <jandress@users.noreply.github.com>
2019-06-19 12:28:12 -0700

    Initial commit

---
+++ sg_audit.py
@@ -0,0 +1,124 @@
+#!/usr/bin/env python
+
+import boto3
+import ConfigParser
+import os
+import csv
+import datetime
+import socket 
+
+def get_ec2_instances(ec2):
+#get the ec2 instances and their associated info
+    #print("in get_ec2_instances")
+    found_instances = {}
+    instance_public_ips = {}
+    instance_private_ips = {}
+    instance_ident = {}
+    #just get the instances that are running
+    instances = ec2.instances.filter(Filters=[{'Name': 'instance-state-name', 'Values': ['running'] }])
+ 
+    #loop through the instances we found and get info on them
+    for instance in instances:
+        if instance.tags:
+            for i in range(len(instance.tags)):
+                if instance.tags[i]['Key'] == "Name":
+                    instance_name = instance.tags[i]['Value']
+        else:
+            instance_name = "unknown"
+        instance_ident[instance.id] = instance_name
+        values=[]
+        for i in range(len(instance.security_groups)):
+            values.append(instance.security_groups[i]['GroupId'])
+            found_instances[instance.id] = values
+            instance_public_ips[instance.id] = instance.public_ip_address
+            instance_private_ips[instance.id] = instance.private_ip_address
+    return (found_instances, instance_public_ips,instance_private_ips, instance_ident)
+ 
+def inspect_security_group(ec2, sg_id):
+#look at the security groups for ports open to the internet
+    #print("in inspect_security_group")
+    sg = ec2.SecurityGroup(sg_id)
+    #print("security group = "+str(sg))
+ 
+    open_cidrs = []
+    for i in range(len(sg.ip_permissions)):
+        to_port = ''
+        ip_proto = ''
+        if 'ToPort' in sg.ip_permissions[i]:
+            to_port = sg.ip_permissions[i]['ToPort']
+            #print("to_port = "+str(to_port))
+            if '-1' in str(to_port):
+                protocol = "All"
+            else:
+                try:
+                    protocol = socket.getservbyport(to_port)
+                except socket.error:
+                    protocol = "unknown"
+        else:
+            protocol = ""
+        #print("protocol = "+protocol)
+        if 'IpProtocol' in sg.ip_permissions[i]:
+            ip_proto = sg.ip_permissions[i]['IpProtocol']
+            if '-1' in ip_proto:
+                ip_proto = 'All'
+        for j in range(len(sg.ip_permissions[i]['IpRanges'])):
+            #cidr_string = "%s %s %s" % (sg.ip_permissions[i]['IpRanges'][j]['CidrIp'], ip_proto, to_port)
+            cidr_string = ip_proto+" "+str(to_port)+"("+protocol+")"
+ 
+            if sg.ip_permissions[i]['IpRanges'][j]['CidrIp'] == '0.0.0.0/0':
+                #preventing an instance being flagged for only ICMP being open
+                if ip_proto != 'icmp':
+                    open_cidrs.append(cidr_string)
+ 
+    return open_cidrs
+ 
+def dump_to_csv(instance_list):
+#dump out everything we found to a csv file
+  print("in dump_to_csv")
+  list_length = len(instance_list[0])
+  d = datetime.datetime.now()
+  timestamp = str(d.date())
+  filename = "sg_audit_"+timestamp+".csv" #output filename
+  with open(filename, 'w') as instance_file:
+        writer = csv.writer(instance_file, lineterminator='\n')
+        #csv header row
+        writer.writerow(['AWS Account','Region', 'Instance ID','Internal IP','External IP','Name','Security Group','Ports open to Internet'])
+        for instance in instance_list:
+           writer.writerow(instance)
+
+if __name__ == "__main__":
+    print("in main")
+    #AWS credentials file goes here, this assumes the currently running user
+    configdir = os.path.expandvars('/home/$USER/.aws/credentials')
+    #print("configdir = "+configdir)
+
+    staticregion = 'us-east-1'
+    instance_list = []
+
+    #read in the sections from the credentials file
+    config = ConfigParser.ConfigParser()
+    config.read(configdir)
+
+    #loop through each section in the credentials file - this allows us to search through multiple AWS accounts
+    for section in config.sections():
+        session = boto3.Session(profile_name=section)
+        client = session.client('ec2', region_name = staticregion)
+        stsclient = session.client('sts', region_name = staticregion)
+
+        regions = [region['RegionName'] for region in client.describe_regions()['Regions']]
+
+        for region in regions:
+            account_id = stsclient.get_caller_identity()["Account"]
+            print("Checking "+section+"("+account_id+")"+" "+region+" ")
+            ec2 = session.resource('ec2', region_name = region)
+            (ec2_instances, instance_public_ips, instance_private_ips, instance_ident) = get_ec2_instances(ec2)
+
+            for instance in ec2_instances:
+                for sg_id in ec2_instances[instance]:
+                    open_cidrs = inspect_security_group(ec2, sg_id)
+                    if open_cidrs: #only print if there are open cidrs
+                        instance_info = account_id, region, instance, instance_private_ips[instance], instance_public_ips[instance], instance_ident[instance], sg_id, open_cidrs
+                        print(instance_info)
+                        instance_list.append(instance_info)
+
+    dump_to_csv(instance_list)
